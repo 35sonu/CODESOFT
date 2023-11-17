d48cc8467d5dbf26f0568a74f12e398f70ad84cf
Sonu Kumar <133563744+35sonu@users.noreply.github.com>
2023-11-17 14:05:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let string =  "";
+let text = document.getElementById('.text');
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button) =>{
+    button.addEventListener('click', (e)=>{
+
+        console.log(e.target)
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value = string;
+
+    })
+
+})
